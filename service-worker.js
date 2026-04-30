d362bb5b36bffdae2bea487c491f40ac3ca481b0
SierraGoldNurseries <gurmail@sgtrees.com>
2026-04-29 20:08:42 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'sgn-structures-app-v7';
+const CACHE = 'sgn-map-app-v10-fix-1';
 const ASSETS = [
   './',
   './index.html',
@@ -15,9 +15,18 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
-    const copy = response.clone();
-    caches.open(CACHE).then((cache) => cache.put(event.request, copy)).catch(() => {});
-    return response;
-  }).catch(() => caches.match('./index.html'))));
+  const url = new URL(event.request.url);
+  const isData = url.pathname.includes('/data/') || /\.(geojson|json|csv)$/i.test(url.pathname);
+  event.respondWith(
+    fetch(event.request).then((response) => {
+      if (response.ok && !isData) {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy)).catch(() => {});
+      }
+      return response;
+    }).catch(() => {
+      if (isData) return new Response(JSON.stringify({ type: 'FeatureCollection', features: [] }), { headers: { 'Content-Type': 'application/json' } });
+      return caches.match(event.request).then((cached) => cached || caches.match('./index.html'));
+    })
+  );
 });
